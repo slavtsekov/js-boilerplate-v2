de3b2518994114087c119cf9d83feac182ff8388
Slav Tsekov <slav.tsekov@gmail.com>
2019-03-24 19:09:47 +0200

    Clear index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-const name = 'test';
-console.log(name);
-
-class TestClass {
-    testMethod() {
-        return 'Testing';
-    }
-}
-const test = new TestClass();
-console.log(test.testMethod());
+console.log('App is running.');
